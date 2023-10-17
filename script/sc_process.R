6559d6be48120dc325b4e8d23774a51a9fe69a45
Shao, Xin <xin_shao@zju.edu.cn>
2023-10-17 16:15:58 +0800

    update

--- script/sc_process.R
+++ script/sc_process.R
@@ -32,14 +32,13 @@ rev_gene_list <- function(rawdata_list, gene_info){
 
 #---------------------------------Process-----------------------------------
 # [1] quality control
-# n_Features < 4000, percent.mt < 25
-# read the raw data with Seurat as a list
+# 200 < n_Features < 4000, percent.mt < 50
 library(Seurat)
 rawdata_list <- readRDS("/path/to/rawdata_list.rds")
 rawdata_list <- lapply(X = rawdata_list, FUN = CreateSeuratObject)
 rawdata_list <- lapply(X = rawdata_list, FUN = function(seurat_data){
   seurat_data[["percent.mt"]] <- PercentageFeatureSet(seurat_data, pattern = "^mt-")
-  seurat_data <- subset(seurat_data, subset = nFeature_RNA < 4000 & percent.mt < 25)
+  seurat_data <- subset(seurat_data, subset = nFeature_RNA < 4000 & percent.mt < 50 & nFeature_RNA > 200)
   return(seurat_data)
 })
 
